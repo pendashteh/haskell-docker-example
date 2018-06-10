d16f0bc208ef3541db267b2028175c25d137a47e
ubeta <ubeta@land>
2018-06-11 08:16:25 +1000

    Better practice for running shell inside Docker.

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -v $(pwd):/root/dev -it --rm haskell:8.2.1 bash -c 'cd /root/dev && "/bin/bash"'
+docker run -v $(pwd):/root/dev -it --rm haskell:8.2.1 bash -c 'cd /root/dev && exec "${SHELL:-sh}"'
